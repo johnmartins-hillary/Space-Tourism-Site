dd93bf38df80b2503433b65764de46a20dd17eac
webdev1kev <webdev1kev@gmail.com>
2021-11-25 21:34:51 -0600

    fixed @media queries in Crew.module.css

--- src/components/SliderBigDots.jsx
+++ src/components/SliderBigDots.jsx
@@ -10,40 +10,42 @@ const SliderBigDots = forwardRef((props, ref) => {
 
   const { list, image, mobileImage } = ref;
 
-  const test = (event) => {
+  const clickHandler = (event) => {
     const clicked = event.target;
 
     setActive(clicked.innerHTML);
     console.log(clicked.dataset.number);
     slideTo(Object.values(list.current.children), "y", clicked.dataset.number);
 
-    mobileImage.current.src = `/${
-      data.technology[clicked.dataset.number].images.landscape
-    }`;
-
-    image.current.src = `/${
-      data.technology[clicked.dataset.number].images.portrait
-    }`;
+    if (mobileImage.current) {
+      mobileImage.current.src = `/${
+        data.technology[clicked.dataset.number].images.landscape
+      }`;
+    } else {
+      image.current.src = `/${
+        data.technology[clicked.dataset.number].images.portrait
+      }`;
+    }
   };
 
   return (
     <div className={`${props.className} ${classes["dot-container"]}`}>
       <button
-        onClick={test}
+        onClick={clickHandler}
         className={active === "1" ? classes.active : ""}
         data-number="0"
       >
         1
       </button>
       <button
-        onClick={test}
+        onClick={clickHandler}
         className={active === "2" ? classes.active : ""}
         data-number="1"
       >
         2
       </button>
       <button
-        onClick={test}
+        onClick={clickHandler}
         className={active === "3" ? classes.active : ""}
         data-number="2"
       >
